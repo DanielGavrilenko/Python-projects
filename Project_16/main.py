e7e7a20800e3eb51db9e1db3ed5f9a5a146e4cbb
Daniel Gavrilenko <156020076+DanielGavrilenko@users.noreply.github.com>
2024-05-21 23:57:55 -0700

    small changes

--- Project_16/main.py
+++ Project_16/main.py
@@ -5,16 +5,16 @@ from money_machine import MoneyMachine
 coffee_machine = CoffeeMaker()
 money = MoneyMachine()
 menu = Menu()
-turn_on = 1
+turn_on = True
 while turn_on:
-    drink_name = input(f"What would you like? ({menu.get_items()}):")
-    if drink_name == "report":
+    choice = input(f"What would you like? ({menu.get_items()}):")
+    if choice == "report":
         coffee_machine.report()
         money.report()
-    elif drink_name == "off":
-        turn_on = 0
+    elif choice == "off":
+        turn_on = False
     else:
-        drink = menu.find_drink(drink_name)
+        drink = menu.find_drink(choice)
         if coffee_machine.is_resource_sufficient(drink):
             if money.make_payment(drink.cost):
                 coffee_machine.make_coffee(drink)
